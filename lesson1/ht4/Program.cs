87b0b473d59f2f05980ccbe57da7c9d13ff5e449
Evgeniy Chernykh <evgechernykh777@yandex.ru>
2023-06-12 18:53:59 +0300

    perform homework for seminar 1

---
+++ lesson1/ht4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите первое целое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье целое число: ");
+int number_3 = int.Parse(Console.ReadLine());
+
+if (number_1 > number_2 & number_1 > number_3)
+{
+    Console.WriteLine($"Максимальное число равно {number_1}");
+}
+else if (number_2 > number_1 & number_2 > number_3)
+{
+    Console.WriteLine($"Максимальное число равно {number_2}");
+}
+else if (number_3 > number_1 & number_3 > number_2)
+{
+    Console.WriteLine($"Максимальное число равно {number_3}");
+}
+else
+{
+    Console.WriteLine("Вы ввели одинаковые числа!");
+}
---
+++ lesson1/ht8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите целое положительное число: ");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+string res = $"Четные числа от {count} до {N}: ";
+
+while (count <= N)
+{
+    if (count % 2 == 0)
+    {
+        res += $"{count}, ";
+    }
+    count++;
+}
+Console.WriteLine(res);
