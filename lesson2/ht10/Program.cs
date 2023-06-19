a61d8e88f7bb436e0b774ea7606c0224bf6ae6fa
Evgeniy Chernykh <evgechernykh777@yandex.ru>
2023-06-19 22:24:15 +0300

    perform homework for seminar 2

---
+++ lesson2/ht10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+if (number_1 < 100 || number_1 >= 1000)
+{
+    Console.WriteLine("Вы ввели некорректное число!");
+}
+else
+{
+    Console.WriteLine($"Вторая цифра цисла равна {number_1 / 10 % 10}");
+}
---
+++ lesson2/ht13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите целое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+while (number_1 > 999)
+{
+    number_1 = number_1 / 10;
+}
+
+if (number_1 < 100)
+{
+    Console.Write("Третьей цифры нет");
+}
+else
+{
+    Console.Write($"Третья цифра числа равна {number_1 % 10}");
+}
