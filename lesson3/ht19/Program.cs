fd7f19de5afc6dcb88f1c34de3a236a6bc9a42c7
Evgeniy Chernykh <evgechernykh777@yandex.ru>
2023-06-21 22:00:04 +0300

    perform homework_3

---
+++ lesson3/ht19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int num_1 = int.Parse(Console.ReadLine());
+
+if (num_1 > 9999 && num_1 < 100000)
+{
+    int num_2 = (num_1 % 10) * 10000 + (num_1 / 10 % 10) * 1000 + (num_1 / 100 % 10) * 100 +
+    (num_1 / 1000 % 10) * 10 + num_1 / 10000 % 10;
+    if (num_2 == num_1)
+    {
+        Console.WriteLine("Введенное число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Введенное число не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели некорректное число!");
+}
