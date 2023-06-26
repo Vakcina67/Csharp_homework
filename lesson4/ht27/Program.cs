a1b095370e6da77e1e39e160818a3ff46c3a0478
Evgeniy Chernykh <evgechernykh777@yandex.ru>
2023-06-26 21:17:14 +0300

    perform homework_4

---
+++ lesson4/ht27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+int Sum_digits(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+Console.WriteLine($"Сумма цифр в числе равна {Sum_digits(number_1)}");
---
+++ lesson4/ht29/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Количество элементов в массиве: ");
+int array_length = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Начальное значение для диапазона: ");
+int num_min = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Конечное значение для диапазона: ");
+int num_max = int.Parse(Console.ReadLine());
+
+int[] Random_array(int array_length, int min, int max)
+{
+    int[] array = new int[array_length];
+    Random random = new Random();
+    for (int i = 0; i < array_length; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]} ");
+    Console.WriteLine("]");
+}
+int[] array = Random_array(array_length, num_min, num_max);
+PrintArray(array);
