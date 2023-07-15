ca2c29ac349ef66d23343d51e9a07b119688eea6
Evgeniy Chernykh <evgechernykh777@yandex.ru>
2023-07-15 22:41:55 +0300

    perform homework_9

---
+++ lesson9/ht66/Program.cs
@@ -0,0 +1,19 @@
+﻿int SumRec(int a, int b)
+{
+    if (a == b) return a;
+    else return a + SumRec(a + 1, b);
+}
+
+
+Console.WriteLine("Введите число M:");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("Первое число не может быть больше второго!");
+    return;
+}
+Console.WriteLine(SumRec(num1, num2));
---
+++ lesson9/ht68/Program.cs
@@ -0,0 +1,20 @@
+﻿int FuncAkkerman(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else if (n != 0 & m == 0) return FuncAkkerman(n - 1, 1);
+    else return FuncAkkerman(n - 1, FuncAkkerman(n, m - 1));
+}
+
+Console.WriteLine("Введите число M:");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 < 0 || num2 < 0)
+{
+    Console.WriteLine("Вы ввели отрицательные числа!");
+    return;
+}
+
+Console.WriteLine(FuncAkkerman(num1, num2));
